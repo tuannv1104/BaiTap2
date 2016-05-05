c9a78da5b587051b8508a4ab3254211997bc836f
tuannv1104 <1312667@student.hcmus.edu.vn>
2016-05-05 11:01:41 +0700

    make it better

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
 module.exports = {
 	
-	'url' : 'mongodb://<tuannv1104@gmail.com>:<viplatoi114>@ds013182.mlab.com:13182/bt2'
+	'url' : 'mongodb://tuannv1104@gmail.com:viplatoi114 @ds013182.mlab.com:13182/bt2'
 }
